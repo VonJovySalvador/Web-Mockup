caf6cdded883e1364bf4005f74371b2f0ac5f281
Von Jovy Salvador <66856462+VonJovySalvador@users.noreply.github.com>
2020-06-29 20:21:00 +0800

    Add files via upload

---
+++ Web Mock-Up/index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>pentaCUBE</title>
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="assets/style/style.css">
+</head>
+<body>
+	<div id="header" class="content">
+		<div class="row">
+			<div class="col-sm-3 header" style="border-top: 3px solid #3c7480;" align="left">
+				<h1>STYLES CONFERENCE</h1>
+			</div>
+
+			<div class="col-sm-9 header" align="right">
+				<ul>
+					<p class="color-two">August 24-26th - Chicago, IL</p>
+
+					<li>
+						<button>HOME</button>
+					</li>
+
+					<li>
+						<button>SPEAKERS</button>
+					</li>
+
+					<li>
+						<button>SCHEDULE</button>
+					</li>
+
+					<li>
+						<button>VENUE</button>
+					</li>
+
+					<li>
+						<button>REGISTER</button>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<br><br>
+
+	<div class="content">
+		<h2 class="color-two content">Dedicated to the Craft of building Website</h2>
+
+		<h5 class="content" style="width: 80%;">Every year the brightest web designers and front-end developers descend on Chicago to discuss the latest tech logies. Join us this August!</h5>
+
+		<button class="register-button">REGISTER NOW</button>
+	</div>
+
+	<br><br>
+
+	<div class="white-box">
+		<div class="content2">
+			<div class="row">
+				<div class="col-sm-4">
+					<p>SPEAKERS</p>
+					<img src="assets/Images/one.jpg">
+					<br><br>
+					<h5>Worl-Class Speakers</h5>
+					<p>Joining us from all around the world are over twenty fantastic speakers, here to share their stories.</p>
+				</div>
+
+				<div class="col-sm-4">
+					<p>SCHEDULE</p>
+					<img src="assets/Images/two.jpg">
+					<br><br>
+					<h5>Three Inspiring Days</h5>
+					<p>Enjoy three inspiring and action-packed days of talks, gatherings, and all-around good times.</p>
+				</div>
+
+				<div class="col-sm-4">
+					<p>VENUE</p>
+					<img src="assets/Images/three.jpg">
+					<br><br>
+					<h5>The Chicago Theatre</h5>
+					<p>Within the heart of downtown Chicago, The Chicago Theatre will provide a beautiful conference venue.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="footer" class="content">
+		<div class="row">
+			<div class="col-sm-3 header" align="left">
+				<p class="color-two">© Styles Conference</p>
+			</div>
+
+			<div class="col-sm-9" align="right">
+				<ul>
+					<li>
+						<button>HOME</button>
+					</li>
+
+					<li>
+						<button>SPEAKERS</button>
+					</li>
+
+					<li>
+						<button>SCHEDULE</button>
+					</li>
+
+					<li>
+						<button>VENUE</button>
+					</li>
+
+					<li>
+						<button>REGISTER</button>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
